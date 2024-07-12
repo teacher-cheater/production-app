5cb22d70345886477f79dcfa98c4866935f9643c
teacher-cheater <johnnykayfun@gmail.com>
2024-07-12 20:55:46 +0300

    add: widget navbar, style links

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,29 +1,28 @@
-import React, {Suspense} from 'react';
-import './styles/index.scss';
-import {Link, Route, Routes} from "react-router-dom";
+import React, { Suspense } from "react";
+import "./styles/index.scss";
+import { Link, Route, Routes } from "react-router-dom";
 import Loading from "pages/Loading";
-import {classNames} from "shared/lib/classNames/classNames";
-import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import { Navbar } from "widgets";
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme();
+  const { theme, toggleTheme } = useTheme();
 
-    return (
-        <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme} >TOGGLE</button>
-            <Link to={'/'}> Главная </Link>
-            <Link to={'/about'}> О сайте </Link>
-            <Suspense fallback={<Loading />}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
-
-        </div>
-    );
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <button onClick={toggleTheme}>TOGGLE</button>
+      <Navbar />
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPage />} />
+          <Route path={"/"} element={<MainPage />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
 
 export default App;
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,13 +1,19 @@
-type Mods = Record<string, boolean | string >
+type Mods = Record<string, boolean | string>;
 
-export function classNames(cls:string, mods: Mods, additional: string[]):string{
-    return [
-        cls,
-        ...additional,
-        ...Object.entries(mods)
-            .filter(([className, value])=> Boolean(value))
-            .map(([className, value])=> className)
-    ].join(' ');
+export function classNames(
+  cls: string,
+  mods: Mods = {},
+  additional: string[] = []
+): string {
+  return [
+    cls,
+    ...additional.filter(Boolean),
+    ...Object.entries(mods)
+      .filter(([className, value]) => Boolean(value))
+      .map(([className, value]) => className),
+  ].join(" ");
 }
 
-classNames('remove-btn', {hovered: true, selectable: true, red: true}, ['pgd']);
+classNames("remove-btn", { hovered: true, selectable: true, red: true }, [
+  "pgd",
+]);

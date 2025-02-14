f680dbf4b48a011571941770e437d7d17ad42dbc
Evgeniy Nekipelov <johnnykayfun@gmail.com>
2025-02-14 21:01:57 +0300

    add: error boundary, page error

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from 'react';
+import Button from "shared/ui/Button/Button";
+
+// компонент для тестирования
+const BugButton = () => {
+    const [error, setError] = useState(false);
+    const onThrow = () => setError(true);
+    useEffect(() => {
+        if (error) throw new Error();
+    }, [error]);
+
+    return (
+        <Button
+            /* eslint-disable-next-line i18next/no-literal-string */
+            onClick={onThrow}>
+            Throw error
+        </Button>
+    );
+};
+
+export default BugButton;
---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {useTranslation} from "react-i18next";
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from "./PageError.module.scss";
+import Button from "shared/ui/Button/Button";
+
+interface PageErrorProps {
+    className?: string
+}
+
+const PageError = ({className}: PageErrorProps) => {
+    const {t} = useTranslation();
+
+    const reloadPage = () => {
+        location.reload();
+    }
+
+    return (
+        <div className={classNames(cls.ErrorPage, {}, [className])}>
+            <p>{t("Произошла непредвиденная ошибка")}</p>
+            <Button onClick={reloadPage}>
+                {t("Обновить страницу")}
+            </Button>
+        </div>
+    );
+};
+
+export default PageError;

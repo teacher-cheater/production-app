8e4d18b3f8438ac42b16e8ff5e3ab5c9b515802d
Evgeniy Nekipelov <johnnykayfun@gmail.com>
2025-02-18 00:14:30 +0300

    fix react testing library

--- src/shared/config/i18n/i18nForTests.ts
+++ src/shared/config/i18n/i18nForTests.ts
@@ -1,19 +1,16 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import {initReactI18next} from 'react-i18next';
 
 i18n
     .use(initReactI18next)
     .init({
         lng: 'ru',
         fallbackLng: 'ru',
-
         debug: false,
-
         interpolation: {
-            escapeValue: false, // not needed for react!!
+            escapeValue: false,
         },
-
-        resources: { ru: { translations: {} } },
+        resources: {ru: {translations: {}}},
     });
 
 export default i18n;

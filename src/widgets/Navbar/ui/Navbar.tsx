63d211cba644663cde1ad49f3e127e4b278b2ce4
Evgeniy Nekipelov <johnnykayfun@gmail.com>
2025-02-25 00:32:53 +0300

    fix navbar, add translation

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,20 +1,22 @@
-import { classNames } from "shared/lib/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
 import cls from "./Navbar.module.scss";
-import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {useTranslation} from "react-i18next";
 
 interface NavbarProps {
-  className?: string;
+    className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => {
+export const Navbar = ({className}: NavbarProps) => {
+    const {t} = useTranslation();
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.links}>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={"/"}>
-                    Главная
+                    {t("Главная")}
                 </AppLink>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={"/about"}>
-                    О сайте
+                    {t("О сайте")}
                 </AppLink>
             </div>
         </div>

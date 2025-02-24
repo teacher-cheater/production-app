e61509e5fa5a4b3b11aa8cc894b062245234a05e
Evgeniy Nekipelov <johnnykayfun@gmail.com>
2025-02-25 01:26:32 +0300

    fix paths

--- config/build/loaders/buildCssLoader.ts
+++ config/build/loaders/buildCssLoader.ts
@@ -1,6 +1,6 @@
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-export function buildCssLoaders(isDev: boolean) {
+export function buildCssLoader(isDev: boolean) {
     return {
         test: /\.s[ac]ss$/i,
         use: [
--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,31 +1,31 @@
-import webpack from "webpack";
-import {BuildPaths} from "../build/types/config";
-import path from "path";
-import {buildCssLoaders} from "../build/loaders/buildCssLoaders";
+import webpack, {RuleSetRule} from 'webpack';
+import path from 'path';
+import {buildCssLoader} from '../build/loaders/buildCssLoader';
+import {BuildPaths} from '../build/types/config';
 
 export default ({config}: { config: webpack.Configuration }) => {
     const paths: BuildPaths = {
         build: '',
         html: '',
         entry: '',
-        src: path.resolve(__dirname, '..', '..', 'src')
-    }
+        src: path.resolve(__dirname, '..', '..', 'src'),
+    };
     config.resolve.modules.push(paths.src);
-    config.resolve.extensions.push('ts', 'tsx');
+    config.resolve.extensions.push('.ts', '.tsx');
 
-    // @ts-ignore
+    // eslint-disable-next-line no-param-reassign
     config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
         if (/svg/.test(rule.test as string)) {
-            return {...rule, exclude: /\.svg$/i}
+            return {...rule, exclude: /\.svg$/i};
         }
         return rule;
-    })
+    });
 
     config.module.rules.push({
         test: /\.svg$/,
-        use: ["@svgr/webpack"],
+        use: ['@svgr/webpack'],
     });
-    config.module.rules.push(buildCssLoaders(true));
+    config.module.rules.push(buildCssLoader(true));
 
     return config;
-}
+};

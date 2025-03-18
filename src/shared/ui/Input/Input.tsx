9514daac7f3fd8982fbbd318760abcec980272f3
Evgeniy Nekipelov <johnnykayfun@gmail.com>
2025-03-19 00:24:24 +0300

    add storybook

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -4,13 +4,13 @@ import React, {
 } from 'react';
 import cls from './Input.module.scss';
 
-type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>;
+type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>
 
 interface InputProps extends HTMLInputProps {
     className?: string;
     value?: string;
-    onChange?: (value: string) => void,
-    autofocus?: boolean
+    onChange?: (value: string) => void;
+    autofocus?: boolean;
 }
 
 export const Input = memo((props: InputProps) => {
@@ -23,16 +23,14 @@ export const Input = memo((props: InputProps) => {
         autofocus,
         ...otherProps
     } = props;
-
     const ref = useRef<HTMLInputElement>(null);
-
     const [isFocused, setIsFocused] = useState(false);
     const [caretPosition, setCaretPosition] = useState(0);
 
     useEffect(() => {
         if (autofocus) {
             setIsFocused(true);
-            ref.current.focus();
+            ref.current?.focus();
         }
     }, [autofocus]);
 
@@ -54,11 +52,8 @@ export const Input = memo((props: InputProps) => {
     };
 
     return (
-        <div
-            className={classNames(cls.InputWrapper, {}, [className])}
-        >
-            {placeholder
-            && (
+        <div className={classNames(cls.InputWrapper, {}, [className])}>
+            {placeholder && (
                 <div className={cls.placeholder}>
                     {`${placeholder}>`}
                 </div>
@@ -75,8 +70,12 @@ export const Input = memo((props: InputProps) => {
                     onSelect={onSelect}
                     {...otherProps}
                 />
-                {isFocused
-                    && <span className={cls.caret} style={{ left: `${caretPosition * 9}px` }} />}
+                {isFocused && (
+                    <span
+                        className={cls.caret}
+                        style={{ left: `${caretPosition * 9}px` }}
+                    />
+                )}
             </div>
         </div>
     );

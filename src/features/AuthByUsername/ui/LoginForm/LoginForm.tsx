0810f9269ee759e16b2d5cf9df61889400cdc32d
Evgeniy Nekipelov <johnnykayfun@gmail.com>
2025-06-23 23:57:30 +0300

    fix global styles

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,15 +1,15 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
-import { Button } from 'shared/ui/Button/Button';
-import { Input } from 'shared/ui/Input/Input';
+import {classNames} from 'shared/lib/classNames/classNames';
+import {useTranslation} from 'react-i18next';
+import {Button, ButtonTheme} from 'shared/ui/Button/Button';
+import {Input} from 'shared/ui/Input/Input';
 import cls from './LoginForm.module.scss';
 
 interface LoginFormProps {
     className?: string;
 }
 
-export const LoginForm = ({ className }: LoginFormProps) => {
-    const { t } = useTranslation();
+export const LoginForm = ({className}: LoginFormProps) => {
+    const {t} = useTranslation();
 
     return (
         <div
@@ -22,12 +22,14 @@ export const LoginForm = ({ className }: LoginFormProps) => {
                 className={cls.input}
             />
             <Input
-                autofocus
                 placeholder={t('Введите пароль')}
                 type="text"
                 className={cls.input}
             />
-            <Button className={cls.loginBtn}>
+            <Button
+                theme={ButtonTheme.OUTLINE}
+                className={cls.loginBtn}
+            >
                 {t('Войти')}
             </Button>
         </div>

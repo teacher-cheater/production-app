421e6b983159a82dcaaab2e28192b177c3cd881d
teacher_cheater <johnnykayfun@gmail.com>
2026-03-11 00:05:43 +0300

    add unit tests (profile slice)

--- src/entities/Profile/model/slice/profileSlice.test.ts
+++ src/entities/Profile/model/slice/profileSlice.test.ts
@@ -1,11 +1,56 @@
-import { profileActions, profileReducer, ProfileSchema } from 'entities/Profile';
+import {
+    profileActions, profileReducer, ProfileSchema, updateProfileData, ValidateProfileError,
+} from 'entities/Profile';
+import { Currency } from 'entities/Currency';
+import { Country } from 'entities/Country';
+
+const data = {
+    first: 'Evgeniy',
+    lastname: 'Nekiy',
+    age: 99,
+    currency: Currency.USD,
+    country: Country.Belarus,
+    city: 'spb',
+    username: 'admin',
+};
 
 describe('profileSlice.test', () => {
-    test('test set username', () => {
+    test('test set readonly', () => {
         const state: DeepPartial<ProfileSchema> = { readonly: false };
         expect(profileReducer(
             state as ProfileSchema,
             profileActions.setReadonly(true),
         )).toEqual({ readonly: true });
+    });
+
+    test('test update profile service pending', () => {
+        const state: DeepPartial<ProfileSchema> = {
+            isLoading: false,
+            validateErrors: [ValidateProfileError.SERVER_ERROR],
+        };
+        expect(profileReducer(
+            state as ProfileSchema,
+            updateProfileData.pending,
+        )).toEqual({
+            isLoading: true,
+            validateErrors: undefined,
+        });
+    });
+
+    test('test update profile service fulfilled', () => {
+        const state: DeepPartial<ProfileSchema> = {
+            isLoading: true,
+        };
+        expect(profileReducer(
+            state as ProfileSchema,
+            updateProfileData.fulfilled(data, ''),
+        )).toEqual({
+            isLoading: false,
+            validateErrors: undefined,
+            readonly: true,
+            validateError: undefined,
+            form: data,
+            data,
+        });
     });
 });

a0a576c969df8c02b5a7676cb9232b9329518ee3
teacher_cheater <johnnykayfun@gmail.com>
2026-03-12 00:41:42 +0300

    add react refresh plugin

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 const MainPage = () => {
     const { t } = useTranslation();
     return (
-        <div>
+        <div style={{ color: 'green' }}>
             <div>{t('Главная страница')}</div>
         </div>
     );
